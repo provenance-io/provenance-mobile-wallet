a8d725c2b1f8578061f85eb77f315f7a18aed194
Jacob Graves <jgraves@figure.com>
2022-01-07 16:58:40 -0700

    Adds a new stats service for some mock data.

---
+++ lib/network/services/stat_service.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+class StatService {
+  static final StatService _singleton = StatService._internal();
+  factory StatService() => _singleton;
+  StatService._internal();
+  static StatService get instance => _singleton;
+
+  // TODO: Do this, but for reals.
+  static Future<OnboardingStat> getStats() async {
+    await Future.delayed(Duration(milliseconds: 500));
+    var random = new Random();
+
+    return OnboardingStat(
+        '\$${random.nextInt(5) + 10}.${random.nextInt(9)}B',
+        random.nextInt(5) + 10,
+        '${random.nextInt(395) + 400}.${random.nextInt(9)}K',
+        '${random.nextInt(6) + 2}.${random.nextInt(99)} sec.');
+  }
+}
+
+class OnboardingStat {
+  String marketCap;
+  int validators;
+  String transactions;
+  String blockTime;
+
+  OnboardingStat(
+      this.marketCap, this.validators, this.transactions, this.blockTime);
+}
