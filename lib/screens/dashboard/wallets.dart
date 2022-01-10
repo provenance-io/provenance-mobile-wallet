9269c83d2de65b51f89895d3b529880f2994af68
Jacob Graves <jgraves@figure.com>
2022-01-10 14:55:50 -0700

    Formatting.

--- lib/screens/dashboard/wallets.dart
+++ lib/screens/dashboard/wallets.dart
@@ -8,13 +8,14 @@ import 'package:flutter_tech_wallet/util/router_observer.dart';
 import 'package:prov_wallet_flutter/prov_wallet_flutter.dart';
 
 class Wallets extends StatefulWidget {
-    @override
+  @override
   State<StatefulWidget> createState() {
     return WalletsState();
   }
 }
 
-class WalletsState extends State<Wallets> with RouteAware, WidgetsBindingObserver {
+class WalletsState extends State<Wallets>
+    with RouteAware, WidgetsBindingObserver {
   List<WalletDetails> wallets = [];
   WalletDetails? selectedWallet;
 
@@ -51,9 +52,8 @@ class WalletsState extends State<Wallets> with RouteAware, WidgetsBindingObserve
       final walletList = await ProvWalletFlutter.getWallets();
       ModalLoadingRoute.dismiss(context);
       setState(() {
-        selectedWallet = walletList
-            .where((element) => element.isSelected)
-            .first;
+        selectedWallet =
+            walletList.where((element) => element.isSelected).first;
         wallets = walletList.where((element) => !element.isSelected).toList();
       });
     });
@@ -63,10 +63,7 @@ class WalletsState extends State<Wallets> with RouteAware, WidgetsBindingObserve
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .otherBackground,
+          backgroundColor: Theme.of(context).colorScheme.otherBackground,
           elevation: 0.0,
           centerTitle: true,
           actions: [
@@ -76,20 +73,13 @@ class WalletsState extends State<Wallets> with RouteAware, WidgetsBindingObserve
                 },
                 child: FwIcon(
                   FwIcons.plus,
-                  color: Theme
-                      .of(context)
-                      .colorScheme
-                      .globalNeutral450,
+                  color: Theme.of(context).colorScheme.globalNeutral450,
                   size: 24,
-                )
-            ),
+                )),
             HorizontalSpacer.medium(),
           ],
-          title: FwText(
-              'Wallets',
-              color: FwColor.globalNeutral550,
-              style: FwTextStyle.h6
-          ),
+          title: FwText('Wallets',
+              color: FwColor.globalNeutral550, style: FwTextStyle.h6),
           leading: IconButton(
             icon: FwIcon(
               FwIcons.back,
@@ -107,47 +97,36 @@ class WalletsState extends State<Wallets> with RouteAware, WidgetsBindingObserve
       final selWallet = selectedWallet!;
 
       return Container(
-          color: Theme
-              .of(context)
-              .colorScheme
-              .otherBackground,
+          color: Theme.of(context).colorScheme.otherBackground,
           child: Padding(
               padding: EdgeInsets.only(top: 40),
-              child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(left: 20, right:
-                        20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            FwText('Selected Wallet',
-                              color: FwColor.globalNeutral550,
-                              style: FwTextStyle.sBold,),
-                          ],
-                        )
-                    ),
-                    VerticalSpacer.small(),
-                    Padding(
-                      padding: EdgeInsets.only(left: 20, right: 20),
-                      child: WalletItem(selWallet, () {
-                        loadWallets();
-                      }),
-                    ),
-                    _showAllWallets(),
-                    Expanded(
-                      child: Container(
-                        color: Theme
-                            .of(context)
-                            .colorScheme
-                            .otherBackground,
-                      ),
-                    )
-                  ]
-              )
-          )
-      );
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        FwText(
+                          'Selected Wallet',
+                          color: FwColor.globalNeutral550,
+                          style: FwTextStyle.sBold,
+                        ),
+                      ],
+                    )),
+                VerticalSpacer.small(),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: WalletItem(selWallet, () {
+                    loadWallets();
+                  }),
+                ),
+                _showAllWallets(),
+                Expanded(
+                  child: Container(
+                    color: Theme.of(context).colorScheme.otherBackground,
+                  ),
+                )
+              ])));
     }
 
     return Container();
@@ -163,16 +142,17 @@ class WalletsState extends State<Wallets> with RouteAware, WidgetsBindingObserve
       children: [
         VerticalSpacer.medium(),
         Padding(
-            padding: EdgeInsets.only(left: 20, right:
-            20),
+            padding: EdgeInsets.only(left: 20, right: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                FwText('All Wallets', color: FwColor.globalNeutral550,
-                  style: FwTextStyle.sBold,),
+                FwText(
+                  'All Wallets',
+                  color: FwColor.globalNeutral550,
+                  style: FwTextStyle.sBold,
+                ),
               ],
-            )
-        ),
+            )),
         VerticalSpacer.medium(),
         Padding(
             padding: EdgeInsets.only(left: 20, right: 20),
@@ -207,7 +187,8 @@ class WalletItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final RenderBox overlay = Overlay.of(context)?.context.findRenderObject() as RenderBox;
+    final RenderBox overlay =
+        Overlay.of(context)?.context.findRenderObject() as RenderBox;
     return Container(
       color: Colors.white,
       child: Column(
@@ -218,72 +199,77 @@ class WalletItem extends StatelessWidget {
             padding: EdgeInsets.only(left: 20, right: 20),
             child: Row(
               children: [
-                FwText(
-                    item.accountName,
-                    color: FwColor.globalNeutral550,
-                    style: FwTextStyle.m
-                ),
+                FwText(item.accountName,
+                    color: FwColor.globalNeutral550, style: FwTextStyle.m),
                 HorizontalSpacer.small(),
                 Container(
                   height: 26,
                   width: 46,
                   decoration: BoxDecoration(
                       color: Color(0xFFEDEDED),
-                      borderRadius: BorderRadius.circular(13.0)
-                  ),
+                      borderRadius: BorderRadius.circular(13.0)),
                   child: Center(
-                      child: FwText(
-                          'Basic',
+                      child: FwText('Basic',
                           color: FwColor.globalNeutral550,
-                          style: FwTextStyle.xs
-                      )
-                  ),
+                          style: FwTextStyle.xs)),
                 ),
                 Expanded(
                   child: Container(),
                 ),
                 GestureDetector(
-                  onTapDown: (details) {
-                    _storePosition(details);
-                  },
+                    onTapDown: (details) {
+                      _storePosition(details);
+                    },
                     onTap: () async {
-                      final result = await showMenu(context: context,
+                      final result = await showMenu(
+                        context: context,
                         position: RelativeRect.fromRect(
-                            (_tapPosition ?? Offset(0,0)) & Size(40, 40), // smaller rect, the touch area
-                            Offset.zero & overlay.size // Bigger rect, the entire screen
-                        ),
+                            (_tapPosition ?? Offset(0, 0)) &
+                                Size(40, 40), // smaller rect, the touch area
+                            Offset.zero &
+                                overlay.size // Bigger rect, the entire screen
+                            ),
                         items: [
-                          if (!item.isSelected) PopupMenuItem<MenuOperation>(
-                            value: MenuOperation.select,
-                            child: FwText('Select'),
-                          ),
+                          if (!item.isSelected)
+                            PopupMenuItem<MenuOperation>(
+                              value: MenuOperation.select,
+                              child: FwText('Select'),
+                            ),
                           PopupMenuItem<MenuOperation>(
                             value: MenuOperation.rename,
                             child: FwText('Rename'),
                           ),
-                          if (!item.isSelected) PopupMenuItem<MenuOperation>(
-                            value: MenuOperation.delete,
-                            child: FwText('Remove'),
-                          ),
-                        ],);
+                          if (!item.isSelected)
+                            PopupMenuItem<MenuOperation>(
+                              value: MenuOperation.delete,
+                              child: FwText('Remove'),
+                            ),
+                        ],
+                      );
 
                       if (result == MenuOperation.rename) {
                         final result = await showDialog(
                             barrierDismissible: false,
                             context: context,
-                            builder: (context) =>
-                                RenameWalletDialog(currentName: item.accountName));
+                            builder: (context) => RenameWalletDialog(
+                                currentName: item.accountName));
                         if (result != null) {
-                          await ProvWalletFlutter.renameWallet(item.id, result as String);
+                          await ProvWalletFlutter.renameWallet(
+                              item.id, result as String);
                           reload.call();
                         }
                       } else if (result == MenuOperation.delete) {
-                        final dialogResult = await FwDialog.showConfirmation(context, message: 'Are you sure you want to remove this wallet?', confirmText: 'Yes', cancelText: 'Cancel');
+                        final dialogResult = await FwDialog.showConfirmation(
+                            context,
+                            message:
+                                'Are you sure you want to remove this wallet?',
+                            confirmText: 'Yes',
+                            cancelText: 'Cancel');
                         if (dialogResult == true) {
                           await ProvWalletFlutter.removeWallet(item.id);
                           reload.call();
                         }
-                    } else if (result == MenuOperation.select) {
+                      } else if (result == MenuOperation.select) {
                         await ProvWalletFlutter.selectWallet(item.id);
                         reload.call();
                       }
@@ -295,13 +281,11 @@ class WalletItem extends StatelessWidget {
                       height: 60,
                       color: Colors.white,
                       child: Center(
-                          child: FwIcon(FwIcons.menuIcon, color: Theme
-                              .of(context)
-                              .colorScheme
-                              .globalNeutral550)
-                      ),
-                    )
-                )
+                          child: FwIcon(FwIcons.menuIcon,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .globalNeutral550)),
+                    ))
               ],
             ),
           ),
@@ -311,17 +295,14 @@ class WalletItem extends StatelessWidget {
               child: Row(
                 children: [
                   FwText(
-                      '${item.address.substring(0, 5)}...${item.address
-                          .substring(30)}',
+                      '${item.address.substring(0, 5)}...${item.address.substring(30)}',
                       color: FwColor.globalNeutral550,
-                      style: FwTextStyle.m
-                  ),
+                      style: FwTextStyle.m),
                   HorizontalSpacer.medium(),
                   GestureDetector(
                     onTap: () {
                       Clipboard.setData(ClipboardData(text: item.address));
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(
+                      ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(content: FwText('Address copied')));
                     },
                     child: Container(
@@ -329,10 +310,7 @@ class WalletItem extends StatelessWidget {
                       height: 24,
                       child: FwIcon(
                         FwIcons.copy,
-                        color: Theme
-                            .of(context)
-                            .colorScheme
-                            .globalNeutral550,
+                        color: Theme.of(context).colorScheme.globalNeutral550,
                         size: 24,
                       ),
                     ),
@@ -346,8 +324,4 @@ class WalletItem extends StatelessWidget {
   }
 }
 
-enum MenuOperation {
-  select,
-  rename,
-  delete
-}
+enum MenuOperation { select, rename, delete }
