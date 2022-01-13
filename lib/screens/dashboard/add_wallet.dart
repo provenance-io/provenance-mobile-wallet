f392a74444a9e0efc0f273e6e211a36e8c5e2101
Jacob Graves <jgraves@figure.com>
2022-01-13 16:14:04 -0700

    Trailing commas in add wallet.

--- lib/screens/dashboard/add_wallet.dart
+++ lib/screens/dashboard/add_wallet.dart
@@ -7,22 +7,26 @@ class AddWallet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.otherBackground,
-          elevation: 0.0,
-          centerTitle: true,
-          title: FwText(Strings.chooseWalletType,
-              color: FwColor.globalNeutral550, style: FwTextStyle.h6),
-          leading: IconButton(
-            icon: FwIcon(
-              FwIcons.back,
-              size: 24,
-              color: Color(0xFF3D4151),
-            ),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.otherBackground,
+        elevation: 0.0,
+        centerTitle: true,
+        title: FwText(
+          Strings.chooseWalletType,
+          color: FwColor.globalNeutral550,
+          style: FwTextStyle.h6,
         ),
-        body: _buildBody(context));
+        leading: IconButton(
+          icon: FwIcon(
+            FwIcons.back,
+            size: 24,
+            color: Color(0xFF3D4151),
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: _buildBody(context),
+    );
   }
 
   Widget _buildBody(BuildContext context) {
