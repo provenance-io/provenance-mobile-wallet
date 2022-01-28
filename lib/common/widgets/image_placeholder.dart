5b1c3db287e13029bed8ccfbeb6d91d14942a8a5
Jacob Graves <jgraves@figure.com>
2022-01-28 14:29:33 -0700

    Adds an ImagePlaceholder.
    It turns out that Color(0xFF9196AA) is actually the theme color globalNeutral350.

---
+++ lib/common/widgets/image_placeholder.dart
@@ -0,0 +1,15 @@
+import 'package:provenance_wallet/common/fw_design.dart';
+
+class ImagePlaceholder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 158,
+      width: 158,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.globalNeutral350,
+        borderRadius: BorderRadius.all(Radius.circular(79)),
+      ),
+    );
+  }
+}
