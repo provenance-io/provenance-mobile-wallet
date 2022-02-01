64e3160fc5b597fbc5ce79d4e0e3564da8118992
Jacob Graves <jgraves@figure.com>
2022-02-01 14:36:17 -0700

    Adds a new service based on the AssetService.

---
+++ lib/network/services/transaction_service.dart
@@ -0,0 +1,88 @@
+import 'package:provenance_wallet/network/models/transaction_response.dart';
+import 'package:provenance_wallet/network/services/base_service.dart';
+import 'package:faker/faker.dart';
+
+class TransactionService {
+  factory TransactionService() => _singleton;
+  TransactionService._internal();
+
+  static final TransactionService _singleton = TransactionService._internal();
+
+  static String get assetServiceBasePathv1 =>
+      '/service-mobile-wallet/external/api/v1/address';
+  static TransactionService get instance => _singleton;
+
+  static Future<BaseResponse<List<TransactionResponse>>> getTransactions(
+    String provenanceAddresses,
+  ) async {
+    final data = await BaseService.instance.GET(
+      '$assetServiceBasePathv1/$provenanceAddresses/transactions',
+      listConverter: (json) {
+        if (json is String) {
+          return <TransactionResponse>[];
+        }
+
+        final List<TransactionResponse> transactions = [];
+
+        try {
+          transactions.addAll(json.map((t) {
+            return TransactionResponse.fromJson(t);
+          }).toList());
+        } catch (e) {
+          return transactions;
+        }
+
+        return transactions;
+      },
+    );
+
+    return data;
+  }
+
+// TODO: Remove me when we get actual data
+  static Future<List<TransactionResponse>> getFakeTransactions(
+    String provenanceAddresses,
+  ) async {
+    final faker = Faker();
+    final List<TransactionResponse> transactions = [];
+
+    for (var i = 0; i < faker.randomGenerator.integer(10, min: 5); i++) {
+      transactions.add(_getFakeTransaction());
+    }
+
+    await Future.delayed(
+      Duration(
+        milliseconds: faker.randomGenerator.integer(1000, min: 500),
+      ),
+    );
+
+    return transactions;
+  }
+
+// TODO: Remove me when we get actual data
+  static TransactionResponse _getFakeTransaction() {
+    var faker = Faker();
+    var amount = faker.randomGenerator.decimal().toStringAsFixed(2);
+
+    return TransactionResponse(
+      address:
+          faker.randomGenerator.fromPatternToHex(['########################']),
+      feeAmount: amount,
+      id: faker.randomGenerator.integer(100000).toString(),
+      signer: faker.person.name(),
+      status: faker.randomGenerator.element([
+        "Cancelled",
+        "Completed",
+      ]),
+      time: faker.date.time(),
+      type: faker.randomGenerator.element(
+        [
+          "Buy",
+          "Purchase",
+          "Deposit",
+          "Withdraw",
+        ],
+      ),
+    );
+  }
+}
