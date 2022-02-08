047b807d61378b4250fee4907dea7803f0f05d36
Jacob Graves <jgraves@figure.com>
2022-02-08 14:46:31 -0700

    I was missing this spacer here.

--- lib/screens/landing/onboarding_landing_slide.dart
+++ lib/screens/landing/onboarding_landing_slide.dart
@@ -45,6 +45,7 @@ class OnboardingLandingSlide extends StatelessWidget {
             color: PwColor.white,
           ),
         ),
+        VerticalSpacer.custom(spacing: 155),
       ],
     );
   }
