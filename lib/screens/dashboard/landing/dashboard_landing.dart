784fe16d94229702047712420003a47d577303c4
Jacob Graves <jgraves@figure.com>
2022-02-09 16:53:12 -0700

    This can now be a stateless widget.

--- lib/screens/dashboard/landing/dashboard_landing.dart
+++ lib/screens/dashboard/landing/dashboard_landing.dart
@@ -1,33 +1,7 @@
 import 'package:provenance_wallet/common/pw_design.dart';
-import 'package:provenance_wallet/network/models/asset_response.dart';
-import 'package:provenance_wallet/screens/dashboard/landing/wallet_connect_no_assets.dart';
-import 'package:provenance_wallet/screens/dashboard/landing/wallet_connect_with_assets.dart';
-import 'package:provenance_wallet/screens/dashboard/landing/wallet_portfolio.dart';
+import 'package:provenance_wallet/screens/dashboard/landing/wallet_connect_section.dart';
 
-class DashboardLanding extends StatefulWidget {
-  DashboardLanding({
-    Key? key,
-    required this.walletKey,
-  }) : super(key: key);
-
-  // FIXME: State Management
-  final GlobalKey<WalletPortfolioState> walletKey;
-
-  @override
-  State<StatefulWidget> createState() => DashboardLandingState(walletKey);
-}
-
-class DashboardLandingState extends State<DashboardLanding> {
-  DashboardLandingState(this.walletKey);
-  List<AssetResponse> _assets = [];
-  GlobalKey<WalletPortfolioState> walletKey;
-
-  void updateAssets(List<AssetResponse> assets) {
-    setState(() {
-      _assets = assets;
-    });
-  }
-
+class DashboardLanding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
