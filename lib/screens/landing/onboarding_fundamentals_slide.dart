69a78ad126afcd26f8edc20c61f0d39a43c0f8c1
Jacob Graves <jgraves@figure.com>
2022-02-09 09:23:34 -0700

    Make a bloc to replace some logic that shouldn't be in a widget.

--- lib/screens/landing/onboarding_fundamentals_slide.dart
+++ lib/screens/landing/onboarding_fundamentals_slide.dart
@@ -1,6 +1,8 @@
 import 'package:provenance_wallet/common/pw_design.dart';
-import 'package:provenance_wallet/network/services/stat_service.dart';
+import 'package:provenance_wallet/util/get.dart';
 import 'package:provenance_wallet/util/strings.dart';
+
+import 'fundamentals_bloc.dart';
 
 class OnboardingFundamentalsSlide extends StatefulWidget {
   @override
@@ -11,39 +13,26 @@ class OnboardingFundamentalsSlide extends StatefulWidget {
 
 class _OnboardingFundamentalsSlideState
     extends State<OnboardingFundamentalsSlide> {
-  String _marketCap = "";
-  int _numValidators = 0;
-  String _transactions = "";
-  String _blockTime = "";
-  bool _isLoading = false;
 
   @override
-  void didChangeDependencies() {
-    getStats();
-    super.didChangeDependencies();
+  void initState() {
+    super.initState();
+
+    get.registerLazySingleton(() => FundamentalsBloc());
+    get<FundamentalsBloc>().load();
   }
 
-  Future<void> getStats() async {
-    setState(() {
-      _isLoading = true;
-    });
+  @override
+  void dispose() {
+    get.unregister<FundamentalsBloc>();
 
-    StatService.getStats().then((stat) {
-      setState(() {
-        _marketCap = stat.marketCap;
-        _transactions = stat.transactions;
-        _numValidators = stat.validators;
-        _blockTime = stat.blockTime;
-        _isLoading = false;
-      });
-    }).onError((error, stackTrace) {
-      // Do something with the error?
-      _isLoading = false;
-    });
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final bloc = get<FundamentalsBloc>();
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -67,83 +56,107 @@ class _OnboardingFundamentalsSlideState
             shrinkWrap: true,
             physics: NeverScrollableScrollPhysics(),
             crossAxisSpacing: 0.0,
-            crossAxisCount: _isLoading ? 1 : 2,
+            crossAxisCount: 2,
             mainAxisSpacing: 0.0,
             childAspectRatio: 1.89,
-            children: _isLoading
-                ? [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [CircularProgressIndicator()],
-                    ),
-                  ]
-                : [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        PwText(
-                          _marketCap,
-                          style: PwTextStyle.h4,
-                          color: PwColor.globalNeutral450,
-                        ),
-                        PwText(
-                          Strings.marketCap,
-                          style: PwTextStyle.m,
-                          color: PwColor.globalNeutral450,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        PwText(
-                          '$_numValidators',
-                          style: PwTextStyle.h4,
-                          color: PwColor.globalNeutral450,
-                        ),
-                        PwText(
-                          Strings.validators,
-                          style: PwTextStyle.m,
-                          color: PwColor.globalNeutral450,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        PwText(
-                          _transactions,
-                          style: PwTextStyle.h4,
-                          color: PwColor.globalNeutral450,
-                        ),
-                        PwText(
-                          Strings.transactions,
-                          style: PwTextStyle.m,
-                          color: PwColor.globalNeutral450,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        PwText(
-                          _blockTime,
-                          style: PwTextStyle.h4,
-                          color: PwColor.globalNeutral450,
-                        ),
-                        PwText(
-                          Strings.avgBlockTime,
-                          style: PwTextStyle.m,
-                          color: PwColor.globalNeutral450,
-                        ),
-                      ],
-                    ),
-                  ],
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<String>(
+                    initialData: bloc.marketCap.value,
+                    stream: bloc.marketCap,
+                    builder: (context, snapshot) {
+                      String marketCap = snapshot.data ?? "";
+
+                      return PwText(
+                        marketCap,
+                        style: PwTextStyle.h4,
+                        color: PwColor.globalNeutral450,
+                      );
+                    },
+                  ),
+                  PwText(
+                    Strings.marketCap,
+                    style: PwTextStyle.m,
+                    color: PwColor.globalNeutral450,
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<int>(
+                    initialData: bloc.validatorsCount.value,
+                    stream: bloc.validatorsCount,
+                    builder: (context, snapshot) {
+                      int numValidators = snapshot.data ?? 0;
+
+                      return PwText(
+                        '$numValidators',
+                        style: PwTextStyle.h4,
+                        color: PwColor.globalNeutral450,
+                      );
+                    },
+                  ),
+                  PwText(
+                    Strings.validators,
+                    style: PwTextStyle.m,
+                    color: PwColor.globalNeutral450,
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<String>(
+                    initialData: bloc.transactions.value,
+                    stream: bloc.transactions,
+                    builder: (context, snapshot) {
+                      String transactions = snapshot.data ?? "";
+
+                      return PwText(
+                        transactions,
+                        style: PwTextStyle.h4,
+                        color: PwColor.globalNeutral450,
+                      );
+                    },
+                  ),
+                  PwText(
+                    Strings.transactions,
+                    style: PwTextStyle.m,
+                    color: PwColor.globalNeutral450,
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<String>(
+                    initialData: bloc.blockTime.value,
+                    stream: bloc.blockTime,
+                    builder: (context, snapshot) {
+                      String blockTime = snapshot.data ?? "";
+
+                      return PwText(
+                        blockTime,
+                        style: PwTextStyle.h4,
+                        color: PwColor.globalNeutral450,
+                      );
+                    },
+                  ),
+                  PwText(
+                    Strings.avgBlockTime,
+                    style: PwTextStyle.m,
+                    color: PwColor.globalNeutral450,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ],
