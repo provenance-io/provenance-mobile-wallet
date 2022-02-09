6554a83a378b7fb3ba2343b137c6dbe37fe34dfc
Jacob Graves <jgraves@figure.com>
2022-02-09 16:26:40 -0700

    Adds the services to GetIt.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provenance_wallet/common/theme.dart';
 import 'package:provenance_wallet/firebase_options.dart';
+import 'package:provenance_wallet/network/services/asset_service.dart';
 import 'package:provenance_wallet/network/services/stat_service.dart';
+import 'package:provenance_wallet/network/services/transaction_service.dart';
 import 'package:provenance_wallet/screens/landing/landing.dart';
 import 'package:provenance_wallet/util/router_observer.dart';
 import 'package:provenance_wallet/util/strings.dart';
@@ -33,6 +35,12 @@ class _ProvenanceWalletAppState extends State<ProvenanceWalletApp> {
 
     get.registerLazySingleton<StatService>(
       () => StatService(),
+    );
+    get.registerLazySingleton<AssetService>(
+      () => AssetService(),
+    );
+    get.registerLazySingleton<TransactionService>(
+      () => TransactionService(),
     );
   }
 
