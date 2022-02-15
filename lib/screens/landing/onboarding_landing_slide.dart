f9240337dc11ae7db90f5c8823ac1041af21e1ba
Jacob Graves <jgraves@figure.com>
2022-02-15 13:51:08 -0700

    Landing slide fixed.

--- lib/screens/landing/onboarding_landing_slide.dart
+++ lib/screens/landing/onboarding_landing_slide.dart
@@ -7,7 +7,7 @@ class OnboardingLandingSlide extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.end,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -18,24 +18,18 @@ class OnboardingLandingSlide extends StatelessWidget {
             ),
           ],
         ),
-        Padding(
-          padding: EdgeInsets.only(
-            left: 20,
-            right: 20,
-            top: 40,
-          ),
-          child: PwText(
-            Strings.provenanceTitle,
-            style: PwTextStyle.logo,
-            textAlign: TextAlign.center,
-            color: PwColor.white,
-          ),
+        VerticalSpacer.largeX3(),
+        PwText(
+          Strings.provenanceTitle,
+          style: PwTextStyle.logo,
+          textAlign: TextAlign.center,
+          color: PwColor.white,
         ),
+        VerticalSpacer.largeX5(),
         Padding(
           padding: EdgeInsets.only(
             left: 20,
             right: 20,
-            top: 64,
           ),
           child: PwText(
             Strings.provenanceWalletDescription,
@@ -44,7 +38,8 @@ class OnboardingLandingSlide extends StatelessWidget {
             color: PwColor.white,
           ),
         ),
-        VerticalSpacer.custom(spacing: 155),
+        VerticalSpacer.largeX6(),
+        VerticalSpacer.largeX5(),
       ],
     );
   }
