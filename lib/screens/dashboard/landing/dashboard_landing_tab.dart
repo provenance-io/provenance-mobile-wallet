195d10508b53db6be6229311d1c4f37352969e0a
Jacob Graves <jgraves@figure.com>
2022-02-17 16:03:09 -0700

    Removes unused import.

--- lib/screens/dashboard/landing/dashboard_landing_tab.dart
+++ lib/screens/dashboard/landing/dashboard_landing_tab.dart
@@ -6,7 +6,6 @@ import 'package:provenance_wallet/screens/dashboard/dashboard_bloc.dart';
 import 'package:provenance_wallet/screens/dashboard/landing/reset_button.dart';
 import 'package:provenance_wallet/screens/dashboard/landing/wallet_portfolio.dart';
 import 'package:provenance_wallet/screens/qr_code_scanner.dart';
-import 'package:provenance_wallet/services/remote_client_details.dart';
 import 'package:provenance_wallet/services/wallet_connection_service_status.dart';
 import 'package:provenance_wallet/util/assets.dart';
 import 'package:provenance_wallet/util/get.dart';
