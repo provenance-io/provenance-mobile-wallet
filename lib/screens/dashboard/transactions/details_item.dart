7913b042291a74650684aa7187d50a7ff6299322
Jacob Graves <jgraves@figure.com>
2022-03-01 16:19:48 -0700

    To make this work in multiple ways the user needs to be able to pass in padding.

--- lib/screens/dashboard/transactions/details_item.dart
+++ lib/screens/dashboard/transactions/details_item.dart
@@ -5,12 +5,14 @@ import 'package:provenance_wallet/common/pw_design.dart';
 class DetailsItem extends StatelessWidget {
   DetailsItem({
     Key? key,
+    this.padding,
     required this.title,
     required this.endChild,
   }) : super(key: key);
 
   DetailsItem.fromStrings({
     Key? key,
+    this.padding,
     required this.title,
     required String value,
   }) : super(key: key) {
@@ -22,16 +24,19 @@ class DetailsItem extends StatelessWidget {
 
   final String title;
   late final Widget endChild;
+  final EdgeInsets? padding;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: Spacing.largeX3,
-        vertical: Spacing.xLarge,
-      ),
+      padding: padding ??
+          EdgeInsets.symmetric(
+            horizontal: Spacing.largeX3,
+            vertical: Spacing.xLarge,
+          ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           PwText(
             title,
