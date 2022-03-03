2a640be69abab2dfa4cae2dd1b8e175258aa561a
Jacob Graves <jgraves@figure.com>
2022-03-03 16:31:20 -0700

    Adds a bloc for the new flow.

---
+++ lib/screens/change_pin_flow/change_pin_bloc.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provenance_wallet/common/pw_design.dart';
+import 'package:provenance_wallet/dialogs/error_dialog.dart';
+import 'package:provenance_wallet/util/get.dart';
+import 'package:provenance_wallet/util/local_auth_helper.dart';
+import 'package:provenance_wallet/util/strings.dart';
+import 'package:rxdart/rxdart.dart';
+
+abstract class ChangePinBlocNavigator {
+  Future<void> confirmPin();
+  Future<void> enableBiometrics();
+  Future<void> pinUpdatedSuccessfully();
+  Future<void> endFlow();
+}
+
+class ChangePinBloc extends Disposable {
+  ChangePinBloc(
+    String accountName,
+    this._navigator,
+  ) : _accountName = BehaviorSubject.seeded(accountName);
+  final ChangePinBlocNavigator _navigator;
+  final BehaviorSubject<String> _accountName;
+  final BehaviorSubject<List<int>> _inputCode = BehaviorSubject.seeded([]);
+
+  final authHelper = get<LocalAuthHelper>();
+
+  ValueStream<String> get accountName => _accountName.stream;
+  ValueStream<List<int>> get inputCode => _inputCode.stream;
+
+  @override
+  FutureOr onDispose() {
+    _accountName.close();
+    _inputCode.close();
+  }
+
+  Future<void> confirmPin(List<int> inputCode) async {
+    _inputCode.value = inputCode;
+    _navigator.confirmPin();
+  }
+
+  Future<void> enableBiometrics(
+    List<int> inputCode,
+    BuildContext context,
+  ) async {
+    if (!ListEquality().equals(inputCode, _inputCode.value)) {
+      await showDialog(
+        useSafeArea: true,
+        context: context,
+        builder: (context) => ErrorDialog(
+          error: Strings.yourPinDoesNotMatchPleaseTryAgain,
+        ),
+      );
+    } else {
+      _navigator.enableBiometrics();
+    }
+  }
+
+  Future<void> enrollInBiometrics(
+    BuildContext context,
+    bool enroll,
+  ) async {
+    authHelper.enroll(
+      _inputCode.value.join(" "),
+      _accountName.value,
+      enroll,
+      context,
+      () async {
+        _navigator.pinUpdatedSuccessfully();
+      },
+    );
+  }
+
+  Future<void> returnToProfile() async {
+    _navigator.endFlow();
+  }
+}
