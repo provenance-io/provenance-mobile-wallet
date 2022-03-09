0a2f60caa1738842836ddf110d90c0c1264221f9
Jacob Graves <jgraves@figure.com>
2022-03-09 10:29:19 -0700

    SingleChildScrollView is now working like it should.

--- lib/screens/dashboard/asset/asset_chart_screen.dart
+++ lib/screens/dashboard/asset/asset_chart_screen.dart
@@ -3,6 +3,7 @@ import 'package:provenance_wallet/common/pw_design.dart';
 import 'package:provenance_wallet/screens/dashboard/asset/asset_bar_chart.dart';
 import 'package:provenance_wallet/screens/dashboard/asset/asset_bar_chart_button.dart';
 import 'package:provenance_wallet/screens/dashboard/asset/asset_chart_bloc.dart';
+import 'package:provenance_wallet/screens/dashboard/asset/asset_chart_recent_transactions.dart';
 import 'package:provenance_wallet/screens/dashboard/asset/asset_chart_statistics.dart';
 import 'package:provenance_wallet/services/asset_service/asset_service.dart';
 import 'package:provenance_wallet/services/models/asset.dart';
@@ -45,100 +46,90 @@ class _AssetChartScreenState extends State<AssetChartScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints viewportConstraints) {
-        return SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: viewportConstraints.maxHeight,
-            ),
-            child: IntrinsicHeight(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(AssetPaths.images.background),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Scaffold(
-                  backgroundColor: Colors.transparent,
-                  appBar: AppBar(
-                    flexibleSpace: Container(
-                      color: Colors.transparent,
-                    ),
-                    backgroundColor: Colors.transparent,
-                    elevation: 0.0,
-                    centerTitle: true,
-                    title: Column(
-                      children: [
-                        SvgPicture.asset(
-                          widget.asset.image,
-                          width: 30,
-                          height: 30,
-                        ),
-                        VerticalSpacer.small(),
-                        PwText(
-                          widget.asset.display.toUpperCase(),
-                          style: PwTextStyle.subhead,
-                        ),
-                      ],
-                    ),
-                    leading: Padding(
-                      padding: EdgeInsets.only(left: 21),
-                      child: IconButton(
-                        icon: PwIcon(
-                          PwIcons.back,
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ),
-                  ),
-                  body: Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: Spacing.xLarge,
-                    ),
-                    color: Colors.transparent,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(bottom: 7),
-                              child: PwText(
-                                '\$',
-                                style: PwTextStyle.h2,
-                                color: PwColor.neutralNeutral,
-                              ),
-                            ),
-                            PwText(
-                              widget.asset.usdPrice.toStringAsFixed(3),
-                              style: PwTextStyle.h1,
-                              color: PwColor.neutralNeutral,
-                            ),
-                          ],
-                        ),
-                        // TODO: This is demo text, to be replaced by a service.
-                        PwText("↑ \$0.008 (0.10%)"),
-                        AssetBarChart(),
-                        VerticalSpacer.medium(),
-                        AssetBarChartButtons(),
-                        VerticalSpacer.xxLarge(),
-                        AssetChartStatistics(),
-                        VerticalSpacer.medium(),
-                      ],
-                    ),
-                  ),
-                ),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AssetPaths.images.background),
+          fit: BoxFit.cover,
+          alignment: Alignment.topCenter,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          flexibleSpace: Container(
+            color: Colors.transparent,
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true,
+          title: Column(
+            children: [
+              SvgPicture.asset(
+                widget.asset.image,
+                width: 30,
+                height: 30,
               ),
+            ],
+          ),
+          leading: Padding(
+            padding: EdgeInsets.only(left: 21),
+            child: IconButton(
+              icon: PwIcon(
+                PwIcons.back,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
           ),
-        );
-      },
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: Spacing.xLarge,
+            ),
+            color: Colors.transparent,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                PwText(
+                  widget.asset.display.toUpperCase(),
+                  style: PwTextStyle.subhead,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 7),
+                      child: PwText(
+                        '\$',
+                        style: PwTextStyle.h2,
+                        color: PwColor.neutralNeutral,
+                      ),
+                    ),
+                    PwText(
+                      widget.asset.usdPrice.toStringAsFixed(3),
+                      style: PwTextStyle.h1,
+                      color: PwColor.neutralNeutral,
+                    ),
+                  ],
+                ),
+                // TODO: This is demo text, to be replaced by a service.
+                PwText("↑ \$0.008 (0.10%)"),
+                AssetBarChart(),
+                VerticalSpacer.medium(),
+                AssetBarChartButtons(),
+                VerticalSpacer.xxLarge(),
+                AssetChartStatistics(),
+                VerticalSpacer.xxLarge(),
+                AssetChartRecentTransactions(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
