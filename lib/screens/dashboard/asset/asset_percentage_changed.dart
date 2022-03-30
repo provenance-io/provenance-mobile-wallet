3c9b0180d3fe4f36a14bf2ce90d7742727e3854a
Jacob Graves <jgraves@figure.com>
2022-03-30 10:55:25 -0600

    Adds new widget for computing percentage changed.

---
+++ lib/screens/dashboard/asset/asset_percentage_changed.dart
@@ -0,0 +1,32 @@
+import 'package:provenance_wallet/common/pw_design.dart';
+import 'package:provenance_wallet/screens/dashboard/asset/asset_chart_bloc.dart';
+import 'package:provenance_wallet/util/get.dart';
+
+class AssetPercentageChanged extends StatelessWidget {
+  const AssetPercentageChanged({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = get<AssetChartBloc>();
+
+    return StreamBuilder<AssetChartDetails?>(
+      initialData: bloc.chartDetails.value,
+      stream: bloc.chartDetails,
+      builder: (context, snapshot) {
+        final list = snapshot.data?.graphItemList;
+        if (list == null || list.length < 2) {
+          return PwText("\$0 (0%)");
+        } else {
+          var last = list.last;
+          var penultimate = list[list.length - 2];
+          final displayPrice = last.price - penultimate.price;
+          final percentageChanged = displayPrice / penultimate.price * 100;
+
+          return PwText(
+            "${percentageChanged.isNegative ? '↓' : '↑'} ${displayPrice.toCurrency()} ($percentageChanged%)",
+          );
+        }
+      },
+    );
+  }
+}
