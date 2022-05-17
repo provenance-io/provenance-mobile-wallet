17cc3f806b062c31fbd533f987861606a1937ad9
Jacob Graves <jgraves@figure.com>
2022-05-17 12:03:13 -0600

    Adds some new service dtos.

---
+++ lib/services/validator_service/dtos/recent_validator_dto.g.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recent_validator_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RecentValidatorDto _$RecentValidatorDtoFromJson(Map<String, dynamic> json) =>
+    RecentValidatorDto(
+      moniker: json['moniker'] as String?,
+      addressId: json['addressId'] as String?,
+      consensusAddress: json['consensusAddress'] as String?,
+      proposerPriority: json['proposerPriority'] as int?,
+      votingPower: json['votingPower'] == null
+          ? null
+          : VotingPowerDto.fromJson(
+              json['votingPower'] as Map<String, dynamic>),
+      commission: json['commission'] as String?,
+      bondedTokens: json['bondedTokens'] == null
+          ? null
+          : BondedTokensDto.fromJson(
+              json['bondedTokens'] as Map<String, dynamic>),
+      delegators: json['delegators'] as int?,
+      status: json['status'] as String?,
+      imgUrl: json['imgUrl'] as String?,
+      hr24Change: json['hr24Change'] as String?,
+      uptime: json['uptime'] as int?,
+    );
+
+Map<String, dynamic> _$RecentValidatorDtoToJson(RecentValidatorDto instance) =>
+    <String, dynamic>{
+      'moniker': instance.moniker,
+      'addressId': instance.addressId,
+      'consensusAddress': instance.consensusAddress,
+      'proposerPriority': instance.proposerPriority,
+      'votingPower': instance.votingPower,
+      'commission': instance.commission,
+      'bondedTokens': instance.bondedTokens,
+      'delegators': instance.delegators,
+      'status': instance.status,
+      'imgUrl': instance.imgUrl,
+      'hr24Change': instance.hr24Change,
+      'uptime': instance.uptime,
+    };
