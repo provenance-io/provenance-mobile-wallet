d1e6f5266873c6a84e4aa1ee0a4aa9fbd0854565
Jacob Graves <jgraves@figure.com>
2022-05-25 09:18:08 -0600

    Detailed Validator dto/model added.

---
+++ lib/services/validator_service/dtos/block_count_dto.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'block_count_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BlockCountDto _$BlockCountDtoFromJson(Map<String, dynamic> json) =>
+    BlockCountDto(
+      count: json['count'] as int?,
+      total: json['total'] as int?,
+    );
+
+Map<String, dynamic> _$BlockCountDtoToJson(BlockCountDto instance) =>
+    <String, dynamic>{
+      'count': instance.count,
+      'total': instance.total,
+    };
---
+++ lib/services/validator_service/dtos/detailed_validator_dto.dart
@@ -0,0 +1,48 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:provenance_wallet/services/validator_service/dtos/block_count_dto.dart';
+import 'package:provenance_wallet/services/validator_service/dtos/voting_power_dto.dart';
+
+part 'detailed_validator_dto.g.dart';
+
+@JsonSerializable()
+class DetailedValidatorDto {
+  DetailedValidatorDto({
+    this.blockCount,
+    this.bondHeight,
+    this.consensusPubKey,
+    this.description,
+    this.identity,
+    this.imgUrl,
+    this.jailedUntil,
+    this.moniker,
+    this.operatorAddress,
+    this.ownerAddress,
+    this.siteUrl,
+    this.status,
+    this.unbondingHeight,
+    this.uptime,
+    this.votingPower,
+    this.withdrawalAddress,
+  });
+  final BlockCountDto? blockCount;
+  final int? bondHeight;
+  final String? consensusPubKey;
+  final String? description;
+  final String? identity;
+  final String? imgUrl;
+  final int? jailedUntil;
+  final String? moniker;
+  final String? operatorAddress;
+  final String? ownerAddress;
+  final String? siteUrl;
+  final String? status;
+  final int? unbondingHeight;
+  final double? uptime;
+  final VotingPowerDto? votingPower;
+  final String? withdrawalAddress;
+
+  // ignore: member-ordering
+  factory DetailedValidatorDto.fromJson(Map<String, dynamic> json) =>
+      _$DetailedValidatorDtoFromJson(json);
+  Map<String, dynamic> toJson() => _$DetailedValidatorDtoToJson(this);
+}
