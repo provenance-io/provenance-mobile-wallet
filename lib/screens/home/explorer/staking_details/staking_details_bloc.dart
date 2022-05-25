e3480975afdc0819217c3f395d460f063b5a434b
Jacob Graves <jgraves@figure.com>
2022-05-25 11:19:53 -0600

    Adds a bloc for the details screen.

---
+++ lib/screens/home/explorer/staking_details/staking_details_bloc.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:get_it/get_it.dart';
+
+class StakingDetailsBloc extends Disposable {
+  @override
+  FutureOr onDispose() {
+    // TODO: implement onDispose
+    throw UnimplementedError();
+  }
+}
