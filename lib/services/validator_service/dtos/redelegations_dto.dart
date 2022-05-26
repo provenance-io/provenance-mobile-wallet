e58374187ce28f6e59fb5a7634f8ec3b4c847297
Jacob Graves <jgraves@figure.com>
2022-05-26 08:33:44 -0600

    Another VSCode problem fix.

--- lib/services/validator_service/dtos/redelegations_dto.dart
+++ lib/services/validator_service/dtos/redelegations_dto.dart
@@ -1,6 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:provenance_wallet/services/validator_service/dtos/delegation_dto.dart';
-import 'package:provenance_wallet/services/validator_service/dtos/delegations_dto.dart';
 
 part 'redelegations_dto.g.dart';
 
