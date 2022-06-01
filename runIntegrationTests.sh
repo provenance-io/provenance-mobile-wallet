cf3a5a46235871ca3cccb32536898c67e973ac81
Roy Dammarell <rdammarell@figure.com>
2022-06-01 15:18:11 -0700

    Update name

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -13,4 +13,4 @@ TEST_DATA_JSON=${TEST_DATA_JSON//$'\n'/}
 # Replace multiple spaces with single space
 TEST_DATA_JSON=$TEST_DATA_JSON | tr -s ' '
 
-flutter test integration_test/recover_wallet_test.dart --flavor dev --dart-define TEST_DATA_JSON="$TEST_DATA_JSON"
+flutter test integration_test/recover_account_test.dart --flavor dev --dart-define TEST_DATA_JSON="$TEST_DATA_JSON"
