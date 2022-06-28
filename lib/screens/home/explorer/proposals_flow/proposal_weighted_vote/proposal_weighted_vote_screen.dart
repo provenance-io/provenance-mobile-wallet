c0a3a01f4ecf5567e9ebb1d5641f4f5ee7ee0817
Jacob Graves <jgraves@figure.com>
2022-06-28 09:40:38 -0600

    TextEditingController removal, since we aren't using this anymore.

--- lib/screens/home/explorer/proposals_flow/proposal_weighted_vote/proposal_weighted_vote_screen.dart
+++ lib/screens/home/explorer/proposals_flow/proposal_weighted_vote/proposal_weighted_vote_screen.dart
@@ -21,63 +21,18 @@ class ProposalWeightedVoteScreen extends StatefulWidget {
 
 class _ProposalDetailsScreenState extends State<ProposalWeightedVoteScreen> {
   late final WeightedVoteBloc _bloc;
-  late final TextEditingController _yesTextEditingController;
-  late final TextEditingController _noTextEditingController;
-  late final TextEditingController _noWithVetoTextEditingController;
-  late final TextEditingController _abstainTextEditingController;
 
   @override
   void initState() {
     _bloc = WeightedVoteBloc();
     get.registerSingleton<WeightedVoteBloc>(_bloc);
-    _yesTextEditingController = TextEditingController(text: "100");
-    _noTextEditingController = TextEditingController(text: "0");
-    _noWithVetoTextEditingController = TextEditingController(text: "0");
-    _abstainTextEditingController = TextEditingController(text: "0");
-    _yesTextEditingController.addListener(_onYesTextChanged);
-    _noTextEditingController.addListener(_onNoTextChanged);
-    _noWithVetoTextEditingController.addListener(_onNoWithVetoTextChanged);
-    _abstainTextEditingController.addListener(_onAbstainTextChanged);
-
     super.initState();
   }
 
   @override
   void dispose() {
     get.unregister<WeightedVoteBloc>();
-    _yesTextEditingController.removeListener(_onYesTextChanged);
-    _noTextEditingController.removeListener(_onNoTextChanged);
-    _noWithVetoTextEditingController.removeListener(_onNoWithVetoTextChanged);
-    _abstainTextEditingController.removeListener(_onAbstainTextChanged);
-    _yesTextEditingController.dispose();
-    _noTextEditingController.dispose();
-    _noWithVetoTextEditingController.dispose();
-    _abstainTextEditingController.dispose();
     super.dispose();
-  }
-
-  void _onYesTextChanged() {
-    _bloc.updateWeight(
-      yesAmount: double.tryParse(_yesTextEditingController.text),
-    );
-  }
-
-  void _onNoTextChanged() {
-    _bloc.updateWeight(
-      noAmount: double.tryParse(_noTextEditingController.text),
-    );
-  }
-
-  void _onNoWithVetoTextChanged() {
-    _bloc.updateWeight(
-      noWithVetoAmount: double.tryParse(_noWithVetoTextEditingController.text),
-    );
-  }
-
-  void _onAbstainTextChanged() {
-    _bloc.updateWeight(
-      abstainAmount: double.tryParse(_abstainTextEditingController.text),
-    );
   }
 
   @override
