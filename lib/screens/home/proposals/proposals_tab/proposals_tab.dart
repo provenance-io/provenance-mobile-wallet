0518070e92a174a7e3d405445e8dd80f97380ec2
Jacob Graves <jgraves@figure.com>
2022-07-13 16:07:30 -0600

    No designs visible unless parent widget is material.

--- lib/screens/home/proposals/proposals_tab/proposals_tab.dart
+++ lib/screens/home/proposals/proposals_tab/proposals_tab.dart
@@ -24,70 +24,70 @@ class ProposalsTabState extends State<ProposalsTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          color: Theme.of(context).colorScheme.neutral750,
-          child: SafeArea(
-            bottom: false,
-            child: StreamBuilder<ProposalDetails>(
-              initialData: _bloc.proposalDetails.value,
-              stream: _bloc.proposalDetails,
-              builder: (context, snapshot) {
-                final stakingDetails = snapshot.data;
-                if (stakingDetails == null) {
-                  return Container();
-                }
+    return Material(
+      child: Stack(
+        children: [
+          Container(
+            color: Theme.of(context).colorScheme.neutral750,
+            child: SafeArea(
+              bottom: false,
+              child: StreamBuilder<ProposalDetails>(
+                initialData: _bloc.proposalDetails.value,
+                stream: _bloc.proposalDetails,
+                builder: (context, snapshot) {
+                  final stakingDetails = snapshot.data;
+                  if (stakingDetails == null) {
+                    return Container();
+                  }
 
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    AppBar(
-                      primary: false,
-                      backgroundColor: Theme.of(context).colorScheme.neutral750,
-                      elevation: 0.0,
-                      title: PwText(
-                        Strings.proposalsTabProposals,
-                        style: PwTextStyle.subhead,
-                      ),
-                      leading: Padding(
-                        padding: EdgeInsets.only(left: 21),
-                        child: IconButton(
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AppBar(
+                        primary: false,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.neutral750,
+                        elevation: 0.0,
+                        title: PwText(
+                          Strings.proposalsTabProposals,
+                          style: PwTextStyle.subhead,
+                        ),
+                        leading: IconButton(
                           icon: PwIcon(
-                            PwIcons.close,
+                            PwIcons.back,
                           ),
                           onPressed: () {
                             Navigator.of(context).pop();
                           },
                         ),
                       ),
-                    ),
-                    ProposalsList(),
-                  ],
-                );
-              },
+                      ProposalsList(),
+                    ],
+                  );
+                },
+              ),
             ),
           ),
-        ),
-        StreamBuilder<bool>(
-          initialData: _bloc.isLoading.value,
-          stream: _bloc.isLoading,
-          builder: (context, snapshot) {
-            final isLoading = snapshot.data ?? false;
-            if (isLoading) {
-              return SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
+          StreamBuilder<bool>(
+            initialData: _bloc.isLoading.value,
+            stream: _bloc.isLoading,
+            builder: (context, snapshot) {
+              final isLoading = snapshot.data ?? false;
+              if (isLoading) {
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
 
-            return Container();
-          },
-        ),
-      ],
+              return Container();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
