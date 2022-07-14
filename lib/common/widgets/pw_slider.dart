1cd0597f601f157d98d905d09022f05edee0347f
Jacob Graves <jgraves@figure.com>
2022-07-14 10:24:41 -0600

    Move the thumb into a new file.

--- lib/common/widgets/pw_slider.dart
+++ lib/common/widgets/pw_slider.dart
@@ -1,4 +1,6 @@
 import 'package:provenance_wallet/common/pw_design.dart';
+import 'package:provenance_wallet/common/widgets/pw_thumb_shape.dart';
+import 'package:provenance_wallet/util/constants.dart';
 import 'package:provenance_wallet/util/strings.dart';
 
 class PwSlider extends StatefulWidget {
@@ -65,7 +67,7 @@ class _PwSliderState extends State<PwSlider> {
                     activeTrackColor: theme.colorScheme.secondary350,
                     inactiveTrackColor: theme.colorScheme.neutral700,
                     overlayColor: theme.colorScheme.neutralNeutral,
-                    thumbShape: _ThumbShape(),
+                    thumbShape: PwThumbShape(),
                     thumbColor: theme.colorScheme.secondary350,
                     overlayShape: RoundSliderOverlayShape(overlayRadius: 5.0),
                     tickMarkShape: RoundSliderTickMarkShape(),
@@ -100,77 +102,6 @@ class _PwSliderState extends State<PwSlider> {
         ),
         VerticalSpacer.large()
       ],
-    );
-  }
-}
-
-class _ThumbShape extends RoundSliderThumbShape {
-  final _indicatorShape = const RectangularSliderValueIndicatorShape();
-  final _innerShape = const RoundSliderThumbShape(enabledThumbRadius: 12);
-
-  const _ThumbShape()
-      : super(
-          disabledThumbRadius: null,
-          elevation: 1.0,
-          enabledThumbRadius: 16,
-          pressedElevation: 8.0,
-        );
-
-  @override
-  void paint(
-    PaintingContext context,
-    Offset center, {
-    required Animation<double> activationAnimation,
-    required Animation<double> enableAnimation,
-    required bool isDiscrete,
-    required TextPainter labelPainter,
-    required RenderBox parentBox,
-    required SliderThemeData sliderTheme,
-    required TextDirection textDirection,
-    required double value,
-    required double textScaleFactor,
-    required Size sizeWithOverflow,
-  }) {
-    assert(sliderTheme.disabledThumbColor != null);
-    assert(sliderTheme.thumbColor != null);
-    super.paint(
-      context,
-      center,
-      activationAnimation: activationAnimation,
-      enableAnimation: enableAnimation,
-      isDiscrete: isDiscrete,
-      labelPainter: labelPainter,
-      parentBox: parentBox,
-      sliderTheme: sliderTheme,
-      textDirection: textDirection,
-      value: value,
-      textScaleFactor: textScaleFactor,
-      sizeWithOverflow: sizeWithOverflow,
-    );
-    _indicatorShape.paint(context, center,
-        activationAnimation: const AlwaysStoppedAnimation(1),
-        enableAnimation: enableAnimation,
-        isDiscrete: isDiscrete,
-        labelPainter: labelPainter,
-        parentBox: parentBox,
-        sliderTheme: sliderTheme,
-        textDirection: textDirection,
-        value: value,
-        textScaleFactor: textScaleFactor,
-        sizeWithOverflow: sizeWithOverflow);
-    _innerShape.paint(
-      context,
-      center,
-      activationAnimation: activationAnimation,
-      enableAnimation: enableAnimation,
-      isDiscrete: isDiscrete,
-      labelPainter: labelPainter,
-      parentBox: parentBox,
-      sliderTheme: sliderTheme.copyWith(thumbColor: Colors.white),
-      textDirection: textDirection,
-      value: value,
-      textScaleFactor: textScaleFactor,
-      sizeWithOverflow: sizeWithOverflow,
     );
   }
 }
