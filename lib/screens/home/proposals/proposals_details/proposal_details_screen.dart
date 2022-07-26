4e9ba6f0c04cb1b575df983b0e2301f04854dbab
Jacob Graves <jgraves@figure.com>
2022-07-26 10:40:31 -0600

    Oops! This needs to come back.

--- lib/screens/home/proposals/proposals_details/proposal_details_screen.dart
+++ lib/screens/home/proposals/proposals_details/proposal_details_screen.dart
@@ -11,6 +11,7 @@ import 'package:provenance_wallet/screens/home/staking/staking_details/details_h
 import 'package:provenance_wallet/screens/home/transactions/details_item.dart';
 import 'package:provenance_wallet/services/models/proposal.dart';
 import 'package:provenance_wallet/services/models/vote.dart';
+import 'package:provenance_wallet/util/constants.dart';
 import 'package:provenance_wallet/util/extensions/string_extensions.dart';
 import 'package:provenance_wallet/util/get.dart';
 import 'package:provenance_wallet/util/strings.dart';
@@ -252,10 +253,10 @@ class _ProposalDetailsScreenState extends State<ProposalDetailsScreen> {
                     ],
                   ),
                 ),
-                // if (_proposal.status.toLowerCase() == votingPeriod)
-                VotingButtons(
-                  proposal: _proposal,
-                ),
+                if (_proposal.status.toLowerCase() == votingPeriod)
+                  VotingButtons(
+                    proposal: _proposal,
+                  ),
               ],
             ),
           ),
