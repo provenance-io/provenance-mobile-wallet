5fc86e3b893310abf1bab6636d1f1d567948f44e
Roy Dammarell <rdammarell@figure.com>
2022-08-30 14:18:56 -0700

    Handle multisig remote messages

--- lib/services/remote_notification/disabled_remote_notification_service.dart
+++ lib/services/remote_notification/disabled_remote_notification_service.dart
@@ -1,6 +1,10 @@
+import 'package:provenance_wallet/services/remote_notification/multi_sig_remote_notification.dart';
 import 'package:provenance_wallet/services/remote_notification/remote_notification_service.dart';
 
 class DisabledRemoteNotificationService implements RemoteNotificationService {
+  @override
+  Stream<MultiSigRemoteNotification> get multiSig => Stream.empty();
+
   @override
   Future<void> registerForPushNotifications(String topic) => Future.value();
 
--- lib/services/remote_notification/remote_notification_service.dart
+++ lib/services/remote_notification/remote_notification_service.dart
@@ -1,4 +1,8 @@
+import 'package:provenance_wallet/services/remote_notification/multi_sig_remote_notification.dart';
+
 abstract class RemoteNotificationService {
+  Stream<MultiSigRemoteNotification> get multiSig;
+
   bool isRegistered(String topic);
 
   Future<void> registerForPushNotifications(
