9e6c90d1959f2368b06fbb1df557624928cb4a5d
Jacob Graves <88213669+jakegraves-figure@users.noreply.github.com>
2022-09-27 10:42:38 -0600

    Easier to combine two blocs into one.

--- lib/screens/home/proposals/proposals_flow.dart
+++ lib/screens/home/proposals/proposals_flow.dart
@@ -8,7 +8,6 @@ import 'package:provenance_wallet/screens/home/proposals/proposal_vote_confirm/p
 import 'package:provenance_wallet/screens/home/proposals/proposal_weighted_vote/proposal_weighted_vote_screen.dart';
 import 'package:provenance_wallet/screens/home/proposals/proposal_weighted_vote_confirm/proposal_weighted_vote_confirm.dart';
 import 'package:provenance_wallet/screens/home/proposals/proposals_details/proposal_details_screen.dart';
-import 'package:provenance_wallet/screens/home/proposals/proposals_flow_bloc.dart';
 import 'package:provenance_wallet/screens/home/proposals/proposals_screen/proposals_bloc.dart';
 import 'package:provenance_wallet/screens/home/proposals/proposals_screen/proposals_screen.dart';
 import 'package:provenance_wallet/services/account_service/account_service.dart';
@@ -16,6 +15,7 @@ import 'package:provenance_wallet/services/models/account.dart';
 import 'package:provenance_wallet/services/models/proposal.dart';
 import 'package:provenance_wallet/util/get.dart';
 import 'package:provenance_wallet/util/strings.dart';
+import 'package:provider/provider.dart';
 
 abstract class ProposalsFlowNavigator {
   Future<void> showProposalDetails(
@@ -69,20 +69,29 @@ class _ProposalsFlowState extends FlowBaseState<ProposalsFlow>
   @override
   void initState() {
     _account = get<AccountService>().events.selected.value!;
-    get.registerSingleton<ProposalsFlowBloc>(ProposalsFlowBloc(this));
-    get.registerSingleton(ProposalsBloc(account: _account)..load());
     super.initState();
   }
 
   @override
   void dispose() {
-    get.unregister<ProposalsFlowBloc>();
     get.unregister<ProposalsBloc>();
     super.dispose();
   }
 
   @override
-  Widget createStartPage() => ProposalsScreen();
+  Widget createStartPage() => Provider<ProposalsBloc>(
+      lazy: true,
+      create: (context) {
+        final bloc = ProposalsBloc(
+          navigator: this,
+          account: _account,
+        )..load();
+        return bloc;
+      },
+      dispose: (_, bloc) {
+        bloc.onDispose();
+      },
+      child: ProposalsScreen());
 
   @override
   Future<void> showProposalDetails(
