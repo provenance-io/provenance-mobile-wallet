3f5d7d567a8132db2c8b60d1b49d2c135c6be6b4
Jacob Graves <88213669+jakegraves-figure@users.noreply.github.com>
2022-09-29 15:04:42 -0600

    Set this one up wrong.

--- lib/screens/home/asset/asset_chart_screen.dart
+++ lib/screens/home/asset/asset_chart_screen.dart
@@ -21,25 +21,24 @@ class AssetChartScreen extends StatefulWidget {
 }
 
 class _AssetChartScreenState extends State<AssetChartScreen> {
-  late AssetChartBloc _bloc;
   final ValueNotifier<AssetChartPointData?> changeNotifier =
       ValueNotifier(null);
 
   @override
   void initState() {
-    _bloc = Provider.of<AssetChartBloc>(context, listen: false);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of<AssetChartBloc>(context, listen: false);
     final mediaQuery = MediaQuery.of(context);
     final isTallScreen = mediaQuery.size.height > 600;
     final priceHeight = (isTallScreen) ? 45.0 : 35.0;
 
     return StreamBuilder<AssetChartDetails?>(
-      initialData: _bloc.chartDetails.value,
-      stream: _bloc.chartDetails,
+      initialData: bloc.chartDetails.value,
+      stream: bloc.chartDetails,
       builder: (context, snapshot) {
         final details = snapshot.data;
         if (null == details) {
@@ -143,7 +142,7 @@ class _AssetChartScreenState extends State<AssetChartScreen> {
                     AssetBarChartButtons(
                       initialValue: details.value,
                       onValueChanged: (GraphingDataValue newValue) {
-                        _bloc.load(
+                        bloc.load(
                           value: newValue,
                         );
                       },
