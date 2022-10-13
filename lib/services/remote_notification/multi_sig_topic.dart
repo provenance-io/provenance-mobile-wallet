464f1fc6522f0810670c8c3cf16141ffd7000115
Roy Dammarell <rdammarell@figure.com>
2022-10-13 14:28:41 -0700

    Show notification for tx declined [sc-187131]

--- lib/services/remote_notification/multi_sig_topic.dart
+++ lib/services/remote_notification/multi_sig_topic.dart
@@ -2,6 +2,7 @@ enum MultiSigTopic {
   accountComplete(id: 'MULTISIG_WALLET_COMPLETE'),
   txSignatureRequired(id: 'MULTISIG_TX_SIGNATURE_REQUIRED'),
   txReady(id: 'MULTISIG_TX_READY'),
+  txDeclined(id: 'MULTISIG_TX_DECLINED'),
   txResult(id: 'MULTISIG_TX_RESULT');
 
   const MultiSigTopic({
