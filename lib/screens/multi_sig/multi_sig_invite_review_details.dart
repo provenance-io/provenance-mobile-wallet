7d83133c0e895b946a7e6010a80f7b79d6f4df65
Jacob Graves <88213669+jakegraves-figure@users.noreply.github.com>
2022-10-17 13:51:56 -0600

    Removes the 'decline' button.

--- lib/screens/multi_sig/multi_sig_invite_review_details.dart
+++ lib/screens/multi_sig/multi_sig_invite_review_details.dart
@@ -39,24 +39,6 @@ class MultiSigInviteReviewDetails extends StatelessWidget {
             },
           ),
         ),
-        actions: [
-          Container(
-            padding: EdgeInsets.only(
-              right: Spacing.large,
-            ),
-            child: PwTextButton.shrinkWrap(
-              child: PwText(
-                Strings.of(context).multiSigInviteReviewDetailsDeclineButton,
-                style: PwTextStyle.body,
-                color: PwColor.primaryP500,
-                textAlign: TextAlign.end,
-              ),
-              onPressed: () async {
-                await bloc.declineInvite();
-              },
-            ),
-          ),
-        ],
         backgroundColor: Theme.of(context).colorScheme.neutral750,
         elevation: 0.0,
         centerTitle: false,
